2e5bac2936591e3f5300c9959d67e275fddf8e17
JAJAmoa <JJ@hgeek.net>
2020-07-25 19:27:37 +0800

    improvement(model): update model arch

--- model/model.py
+++ model/model.py
@@ -11,7 +11,6 @@ class PHNet(nn.Module):
         super(PHNet, self).__init__()
         self.seen = 0
         self.frame = 3
-        self.conv3d = nn.Conv3d(3, 3, kernel_size=3, padding=1,  stride=(1,1,1))
         self.conv11 = nn.Conv3d(in_channels=3, out_channels=3, kernel_size=(self.frame + 1,1,1), stride=1)
         self.frontend_feat = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512]
         self.frontend_feat2 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512]
@@ -22,10 +21,7 @@ class PHNet(nn.Module):
         self.backend = make_layers(self.backend_feat, in_channels = 512, dilation = 2)
         self.backend2 = make_layers(self.backend_feat2, in_channels = 512, dilation = 2)
         self.output_layer = nn.Conv2d(192, 1, kernel_size=1)
-        self.CRPool1 = nn.AvgPool3d(kernel_size=(2, 3, 3), stride=1, padding=(1, 1, 1))
-        self.CRPool2 = nn.AvgPool3d(kernel_size=(3, 1, 1), stride=1, padding=(0, 0, 0))
-        self.BN = nn.BatchNorm3d(3, affine=True, momentum=.95)
-        self.BN2 = nn.BatchNorm2d(64, affine=True, momentum=.95)
+        self.CRPool = nn.Conv3d(3, 3, kernel_size=(2, 3, 3), stride=1, padding=(1, 1, 1), bias=False)
         self.relu = nn.ReLU()
         if not load_weights:
             mod = models.vgg16(pretrained = True)
@@ -37,7 +33,7 @@ class PHNet(nn.Module):
 
     def forward(self,x):
         y = x.clone()
-        y = self.CRPool1(y)
+        y = self.CRPool(y)
         y = self.conv11(y*y)
         y = torch.squeeze(y, dim = 2)
         y = self.frontend2(y)
@@ -58,8 +54,11 @@ class PHNet(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Conv3d):
-                nn.init.normal_(m.weight, std=0.01)
+            elif (isinstance(m, nn.Conv3d)):
+                if m.bias is not None:
+                    nn.init.normal_(m.weight, std=0.01)
+                else:
+                    nn.init.constant_(m.weight, float(1/18/self.frame))
 
 
 def make_layers(cfg, in_channels = 3,batch_norm=False,dilation = False):
@@ -83,7 +82,7 @@ def make_layers(cfg, in_channels = 3,batch_norm=False,dilation = False):
 
 def main():
     model=PHNet()
-    x = torch.rand(2, 5, 600, 400)
+    x = torch.rand(1, 3, 3, 600, 400)
     x = model(x)
     print(x.shape)
 
